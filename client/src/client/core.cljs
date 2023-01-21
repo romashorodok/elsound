8acdef969c678a8d6333d2b9d13326fbe9db835e
romashorodok <108732029+romashorodok@users.noreply.github.com>
2023-01-21 19:28:44 +0200

    feat: Fetch first chunk.

--- client/src/client/core.cljs
+++ client/src/client/core.cljs
@@ -4,7 +4,10 @@
    [reagent.core :as r]
    [reagent.dom :as rd]
    [goog.object :refer [get]]
-   ["react-dom/client" :refer [createRoot]]))
+   ["react-dom/client" :refer [createRoot]]
+   [clojure.string :as s]
+   [cljs.core.async :refer [go]]
+   [cljs.core.async.interop :refer-macros [<p!]]))
 
 (defn destruct [o]
   (reify
@@ -30,22 +33,62 @@
 (defn subscribe [^js/EventTarget target type fn opts]
   (.addEventListener target (key->js type) fn opts))
 
+;; Should it be better like macro ?
+(def subscribe-option!-history (atom nil))
+(defn subscribe-option!
+  ([^Atom target type fn]
+   (subscribe-option! target type fn {}))
+  ([^Atom target type fn opts]
+   (add-watch target (keyword type)
+              #(do
+                 (when-let [previous (get-in @subscribe-option!-history [(keyword type)])]
+                   (.removeEventListener previous (key->js type) fn))
+                 (swap! subscribe-option!-history merge {(keyword type) %4})
+                 
+                 (when-let [target @target]
+                   (subscribe target type fn opts))))))
+
 (defn unmount-source-buffer [media buffer]
   (when buffer
     (.removeSourceBuffer media buffer)))
+
+(defn header->content-range [resp]
+  (when-let [content-range (-> resp .-headers (.get "content-range"))]
+    (let [range-meta       (s/split content-range " ")
+          ranges-with-size (s/split (first (rest range-meta)) "/")
+          ranges           (s/split (first ranges-with-size) "-")]
+      {:unit  (first range-meta)
+       :start (first ranges)
+       :end   (last ranges)
+       :size  (last ranges-with-size)})))
+
+(defn fetch-buffer [{:keys [url header]}]
+  (-> (js/fetch url (clj->js {:headers header}))
+      (.then #(hash-map :content-range (header->content-range %)
+                        :buffer  (.arrayBuffer %)))))
 
 (defn MediaSource [{:keys [type] :or {type "audio/mpeg"}}]
   (let [media         (js/MediaSource.)
         source-ref    (cljs.core/atom nil)
         source-buffer (cljs.core/atom nil)]
-    
-    (add-watch source-buffer :test #(tap> @source-buffer))
 
     (subscribe media :sourceopen
                (fn [e]
-                 (let [{media :target} (destruct e)]
-                   (reset! source-buffer (.addSourceBuffer media type))))
+                 (let [{media :target} (destruct e)
+                       -source-buffer  (.addSourceBuffer media type)]
+                   (reset! source-buffer -source-buffer)
+                   (go
+                     (let [resp       (<p! (fetch-buffer {:url stream :header {:range "bytes=0-"}}))
+                           buffer-end (-> resp :content-range :end)]
+                       (set! (.-appendWindowEnd -source-buffer) buffer-end)
+                       (.appendBuffer -source-buffer (<p! (:buffer resp)))))))
+               
                #js{:once true})
+
+    (subscribe-option! source-buffer :update
+                       (fn [e]
+                         (let [{source-buffer :target} (destruct e)]
+                           (tap> source-buffer))))
     
     (r/create-class
       {:component-did-mount
@@ -61,7 +104,7 @@
   (r/with-let [audio (r/atom true)]
     [:<>
      (if @audio
-       [:audio
+       [:audio {:controls true}
         [MediaSource audio]])
      [:button {:on-click #(swap! audio not)} "Change audio"]]))
 
